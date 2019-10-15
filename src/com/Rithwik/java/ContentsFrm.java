f3151237b96b81c60fe90da5cfb675877c1ebfd7
pwater44 <56414116+pwater44@users.noreply.github.com>
2019-10-15 16:59:29 -0400

    first commit

---
+++ src/com/Rithwik/java/ContentsFrm.java
@@ -0,0 +1,19 @@
+package com.Rithwik.java;
+
+import javax.swing.JFrame;
+
+public class ContentsFrm extends JFrame {
+	
+	public ContentsFrm() {
+		setSize(300, 300);
+		setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		new ContentsFrm();
+		
+	
+	}
+
+}
